d49e6a42e9d6c5cf0678c47250b866cb82c5cec7
Fleckenstein <25FleckensteinD@badinhs.org>
2025-03-03 13:42:37 -0500

    For rock, paper, scissor game

--- rps.js
+++ rps.js
@@ -1,2 +1,37 @@
 // "For rock, paper, scissor game"
 
+/* Plan is to once game gets opened ask player to pick rock, paper, scissors then computer will pic weather or not the option the player 
+picked will beat the one it picked
+Example: (Player) - picked: Rock (computer) - Picked: Paper computer wins. if the player won or loss if won say "Congrats You Are Winner" 
+if lose say "You are Bad You Are Loser" */
+
+const Paper = 1;
+const Rock = 2;
+const Scissors = 3;
+let PlayerChoice = undefined;
+let BotChoice = undefined;
+let input = undefined;
+function Playerinput(){
+    input = prompt("Rock, Paper, Scissors");
+
+switch(input){
+    case "Rock":
+        PlayerChoice = 1;
+        break;
+    case "Paper":
+        PlayerChoice = 2;
+        break;
+    case "Scissors":
+        PlayerChoice = 3;
+        break;
+    default:
+        Playerinput()
+        break;
+    }
+}
+Playerinput()
+function botinput(max){
+    Botchoice = Math.ceil(Math.random() * max)
+    return Botchoice
+}
+
